bc7edc9af8348197a998b8e06060a0aec324970f
Manuel Rodriguez <mrodriguezrodriguez@cifpfbmoll.eu>
2024-02-19 15:47:03 +0100

    Ejercicio Vue.js

---
+++ cancionesvue/src/components/Tarjeta.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="card">
+            <h1 class="card-title">{{cancion.titulo}}</h1>
+        <div class="card-body">
+            <p class="card-text">{{cancion.fecha}}</p>
+        </div>
+      <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+props: {
+    cancion: Object,
+},
+};
+</script>
